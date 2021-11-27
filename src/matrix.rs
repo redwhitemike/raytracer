097589609b2e1ce8cd0c41408a9061a3b741bf7a
Maciek Mika <maciekmika@hotmail.com>
2021-11-27 19:03:13 +0100

    added comparing matrixes

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,5 +1,6 @@
 use num::Float;
 
+#[derive(Debug)]
 struct Matrix<T>
 where
     T: Float,
@@ -12,31 +13,23 @@ where
 impl<T> Matrix<T>
 where
     T: Float,
+    T: PartialEq,
 {
-    // create a new Matrix of 2x2 dimensions
-    pub fn new_2x2() -> Self {
+    // create a new matrix with given size
+    pub fn new(size: usize) -> Self {
         Self {
-            data: vec![T::zero(); 2 * 2],
-            rows: 2,
-            cols: 2,
+            data: vec![T::zero(); size * size],
+            rows: size,
+            cols: size,
         }
     }
 
-    // create a new Matrix of 3x3 dimensions
-    pub fn new_3x3() -> Self {
+    // create a matrix from a given vector of floats
+    pub fn new_from(size: usize, data: Vec<T>) -> Self {
         Self {
-            data: vec![T::zero(); 3 * 3],
-            rows: 3,
-            cols: 3,
-        }
-    }
-
-    // create a new Matrix of 4x4 dimensions
-    pub fn new_4x4() -> Self {
-        Self {
-            data: vec![T::zero(); 4 * 4],
-            rows: 4,
-            cols: 4,
+            data,
+            rows: size,
+            cols: size,
         }
     }
 
@@ -46,13 +39,21 @@ where
     }
 }
 
+// implement '==' for Matrix<T>. Make it possible to check if
+// 2 matrixes are the same
+impl<T: Float> PartialEq for Matrix<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.data == other.data && self.rows == other.rows && self.cols == self.cols
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::matrix::Matrix;
 
     #[test]
     fn create_matrix_4x4() {
-        let mut matrix: Matrix<f64> = Matrix::new_4x4();
+        let mut matrix: Matrix<f64> = Matrix::new(4);
         let matrix_values: Vec<f64> = vec![
             1.0, 2.0, 3.0, 4.0, 5.5, 6.5, 7.5, 8.5, 9.0, 10.0, 11.0, 12.0, 13.5, 14.5, 15.5, 16.5,
         ];
@@ -69,7 +70,7 @@ mod tests {
 
     #[test]
     fn create_matrix_3x3() {
-        let mut matrix: Matrix<f64> = Matrix::new_3x3();
+        let mut matrix: Matrix<f64> = Matrix::new(3);
         let matrix_values: Vec<f64> = vec![-3.0, 5.0, 0.0, 1.0, -2.0, -7.0, 0.0, 1.0, 1.0];
         matrix.data = matrix_values;
 
@@ -80,7 +81,7 @@ mod tests {
 
     #[test]
     fn create_matrix_2x2() {
-        let mut matrix: Matrix<f64> = Matrix::new_2x2();
+        let mut matrix: Matrix<f64> = Matrix::new(2);
         let matrix_values: Vec<f64> = vec![-3.0, 5.0, 1.0, -2.0];
         matrix.data = matrix_values;
 
@@ -88,5 +89,43 @@ mod tests {
         assert_eq!(*matrix.get(0, 1).unwrap(), 5.0);
         assert_eq!(*matrix.get(1, 0).unwrap(), 1.0);
         assert_eq!(*matrix.get(1, 1).unwrap(), -2.0);
+    }
+
+    #[test]
+    fn compare_matrix_equal() {
+        let matrix1: Matrix<f64> = Matrix::new_from(
+            4,
+            vec![
+                1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0,
+            ],
+        );
+
+        let matrix2: Matrix<f64> = Matrix::new_from(
+            4,
+            vec![
+                1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0,
+            ],
+        );
+
+        assert_eq!(matrix1, matrix2)
+    }
+
+    #[test]
+    fn compare_matrix_not_equal() {
+        let matrix1: Matrix<f64> = Matrix::new_from(
+            4,
+            vec![
+                1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0,
+            ],
+        );
+
+        let matrix2: Matrix<f64> = Matrix::new_from(
+            4,
+            vec![
+                2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0,
+            ],
+        );
+
+        assert_ne!(matrix1, matrix2)
     }
 }
