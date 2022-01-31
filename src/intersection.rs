1fd3cee3d82a3f4b44d9cf75b49ccdcb41b8131f
Maciek Mika <maciekmika@hotmail.com>
2022-01-31 14:26:09 +0100

    added tests for intersections, added index and indexMut trait to intersections

--- src/intersection.rs
+++ src/intersection.rs
@@ -34,8 +34,40 @@ where
     data: [Intersection<T, O>; N],
 }
 
+impl<T, O, const N: usize> Intersections<T, O, N>
+where
+    T: Float,
+    O: IntersectionObject,
+{
+    pub fn new(data: [Intersection<T, O>; N]) -> Self {
+        Self { data }
+    }
+}
+
+impl<T, O, const N: usize> Index<usize> for Intersections<T, O, N>
+where
+    T: Float,
+    O: IntersectionObject,
+{
+    type Output = Intersection<T, O>;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.data[index]
+    }
+}
+
+impl<T, O, const N: usize> IndexMut<usize> for Intersections<T, O, N>
+where
+    T: Float,
+    O: IntersectionObject,
+{
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self[index]
+    }
+}
+
 mod tests {
-    use crate::intersection::Intersection;
+    use crate::intersection::{Intersection, Intersections};
     use crate::sphere::Sphere;
 
     #[test]
@@ -45,5 +77,27 @@ mod tests {
 
         assert_eq!(intersection.value, 1.0);
         assert_eq!(intersection.object, sphere)
+    }
+
+    #[test]
+    fn create_intersections_check_values() {
+        let sphere = Sphere::new(1);
+        let intersection_1 = Intersection::<f64, Sphere>::new(1.0, sphere.clone());
+        let intersection_2 = Intersection::<f64, Sphere>::new(2.0, sphere.clone());
+        let intersections = Intersections::<f64, Sphere, 2>::new([intersection_1, intersection_2]);
+
+        assert_eq!(intersections[0].value, 1.0);
+        assert_eq!(intersections[1].value, 2.0)
+    }
+
+    #[test]
+    fn create_intersection_check_objects() {
+        let sphere = Sphere::new(1);
+        let intersection_1 = Intersection::<f64, Sphere>::new(1.0, sphere.clone());
+        let intersection_2 = Intersection::<f64, Sphere>::new(2.0, sphere.clone());
+        let intersections = Intersections::<f64, Sphere, 2>::new([intersection_1, intersection_2]);
+
+        assert_eq!(intersections[0].object, sphere);
+        assert_eq!(intersections[1].object, sphere)
     }
 }
