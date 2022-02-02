e874094feca191f1e6c4b3756b412896d71ef8b6
Maciek Mika <maciekmika@hotmail.com>
2022-02-02 13:38:25 +0100

    fixed intersect with scaled sphere ray test

--- src/sphere.rs
+++ src/sphere.rs
@@ -10,7 +10,7 @@ where
     T: Float,
 {
     id: i32,
-    translation: Matrix<T, 4>,
+    transformation: Matrix<T, 4>,
 }
 
 impl<T> Sphere<T>
@@ -21,8 +21,14 @@ where
     pub fn new(id: i32) -> Self {
         Self {
             id,
-            translation: Matrix::<T, 4>::identity_matrix(),
+            transformation: Matrix::<T, 4>::identity_matrix(),
         }
+    }
+
+    // made it a function for now, maybe
+    // more functionality is added in the future
+    pub fn set_translation(&mut self, transformation: Matrix<T, 4>) {
+        self.transformation = transformation
     }
 }
 
@@ -31,10 +37,17 @@ where
 impl<T> IntersectionObject<T> for Sphere<T>
 where
     T: Float,
+    T: AddAssign,
 {
     type Object = Sphere<T>;
 
     fn intersect(&self, ray: Ray<T>) -> Result<[Intersection<T, Self::Object>; 2], &'static str> {
+        let mut inverse = self.transformation.clone();
+        match inverse.inverse() {
+            Ok(inv) => inverse = inv,
+            Err(_) => return Err("inverse failed"),
+        }
+        let ray = ray.transform(inverse);
         let sphere_to_ray = ray.origin - Tuple::<T>::new_point(T::zero(), T::zero(), T::zero());
         let a = ray.direction.dot_product(&ray.direction);
         let b: T = ray.direction.dot_product(&sphere_to_ray) * T::from(2.0).unwrap();
@@ -60,10 +73,10 @@ where
 }
 
 mod tests {
-    use crate::intersection::IntersectionObject;
+    use crate::intersection::{Intersection, IntersectionObject};
     use crate::ray::Ray;
     use crate::sphere::Sphere;
-    use crate::Tuple;
+    use crate::{Matrix, Tuple};
 
     #[test]
     fn ray_intersection_2_points() {
@@ -162,5 +175,36 @@ mod tests {
     #[test]
     fn sphere_default_transformation() {
         let sphere = Sphere::<f64>::new(1);
+        let identity_matrix = Matrix::<f64, 4>::identity_matrix();
+
+        assert_eq!(sphere.transformation, identity_matrix)
+    }
+
+    #[test]
+    fn change_sphere_transformation() {
+        let mut sphere = Sphere::<f64>::new(1);
+        let translation = Matrix::<f64, 4>::translation(2.0, 3.0, 4.0);
+        sphere.set_translation(translation.clone());
+
+        assert_eq!(sphere.transformation, translation)
+    }
+
+    #[test]
+    fn intersect_scaled_sphere_ray() {
+        let ray = Ray::<f64>::new(
+            Tuple::<f64>::new_point(0.0, 0.0, -5.0),
+            Tuple::<f64>::new_vector(0.0, 0.0, 1.0),
+        );
+        let mut sphere = Sphere::<f64>::new(1);
+        sphere.set_translation(Matrix::scaling(2.0, 2.0, 2.0));
+        match sphere.intersect(ray) {
+            Ok(inter) => {
+                assert_eq!(inter[0].value, 3.0);
+                assert_eq!(inter[1].value, 7.0)
+            }
+            Err(_) => {
+                assert_eq!(true, false)
+            }
+        }
     }
 }
